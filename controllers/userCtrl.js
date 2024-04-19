e6b076203102f1aaf456ff87999240bdde567dae
Manish Kumar <mk1849253@gmail.com>
2024-04-20 04:14:51 +0530

    login page implemented

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,5 +1,6 @@
 const userModel = require("../models/userModels");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const registerController = async (req, res) => {
   try {
